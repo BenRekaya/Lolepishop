6c1dbc6cdde7fdebe7c9a30c5f14162b74ad25a1
Med BenRekaya <benrekayamohamed1@gmail.com>
2022-02-19 12:50:14 +0100

    First commit

---
+++ Lolepichop/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Lolepichop
+//
+//  Created by Mohamed Ben Rekaya on 19/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        
+    
+    }
+
+
+}
+
