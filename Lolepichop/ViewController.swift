97cc11bc68cb77fee095f13a5323481379bd3a47
Med BenRekaya <benrekayamohamed1@gmail.com>
2022-02-19 13:02:47 +0100

    testing commit

--- Lolepichop/ViewController.swift
+++ Lolepichop/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var greeting: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
-        
+        self.greeting = "Hello world ?"
     
     }
 
